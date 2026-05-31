648d6fd2a61aa3ea0270d5e80d5c6331ecb773c7
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-06-01 04:16:02 +0900

    [UPDATE]테스트용 로그인 성공 alert 삭제

--- src/features/auth/components/login/LoginForm.tsx
+++ src/features/auth/components/login/LoginForm.tsx
@@ -28,7 +28,6 @@ export default function LoginForm() {
         password: userPasswordInput,
       });
 
-      alert(`🎉 ${name}님, 환영합니다!`);
       window.location.href = "/";
 
     } catch (error: any) {
