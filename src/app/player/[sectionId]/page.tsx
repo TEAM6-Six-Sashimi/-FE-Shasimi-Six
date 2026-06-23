a51ced6c213ccac9904d00cf30038a1b48a80dbb
always62442 <always22@g.eulji.ac.kr>
2026-06-23 17:28:37 +0900

    [FEATURE] 강의 영상 재생 페이지 제작

--- src/app/player/[sectionId]/page.tsx
+++ src/app/player/[sectionId]/page.tsx
@@ -1,5 +1,15 @@
-export default function VideoPage() {
-    return (
-        <div>VideoPage</div>
-    );
-}   
+import PlayerPage from './components/PlayerPage';
+
+interface Props {
+  params: Promise<{ sectionId: string }>;
+}
+
+export default async function Page({ params }: Props) {
+  const { sectionId } = await params;
+
+  return (
+    <div className="min-h-[calc(100vh-3rem)] bg-[#F9FAFB]">
+      <PlayerPage sectionId={sectionId} />
+    </div>
+  );
+}
