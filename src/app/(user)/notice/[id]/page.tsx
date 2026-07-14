dfbd34931e21c50738637526c6f16f7dea160b73
always62442 <always22@g.eulji.ac.kr>
2026-07-14 15:54:45 +0900

    [FEATURE] 공지사항 상세 페이지 제작 및 api 연동

--- src/app/(user)/notice/[id]/page.tsx
+++ src/app/(user)/notice/[id]/page.tsx
@@ -1,5 +1,19 @@
-export default function NoticeDetailPage() {
-    return (
-        <div>공지사항 상세 조회</div>
-    );
+import { fetchNoticeDetail } from '@/services/notice.service';
+import NoticeDetailView from '@/features/user/notice/components/NoticeDetailView';
+
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function NoticeDetailPage({ params }: PageProps) {
+  const { id } = await params;
+  const result = await fetchNoticeDetail(Number(id));
+
+  return (
+    <div className="bg-[#F9FAFB]">
+      <div className="max-w-5xl min-h-screen mx-auto px-6 py-10">
+        <NoticeDetailView result={result} />
+      </div>
+    </div>
+  );
 }
