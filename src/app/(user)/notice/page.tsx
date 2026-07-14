613ee2d640f419a87850b9ca465880add3cf2a9d
always62442 <always22@g.eulji.ac.kr>
2026-07-14 16:18:00 +0900

    [FEATURE] 공지사항 전체 목록 페이지 제작 및 api 연동

--- src/app/(user)/notice/page.tsx
+++ src/app/(user)/notice/page.tsx
@@ -1,5 +1,11 @@
+import NoticeListView from '@/features/user/notice/components/NoticeListView';
+
 export default function NoticeListPage() {
-    return (
-        <div>공지사항 목록</div>
-    );
+  return (
+    <div className="bg-[#F9FAFB]">
+      <div className="max-w-5xl mx-auto min-h-screen px-6 py-10">
+        <NoticeListView />
+      </div>
+    </div>
+  );
 }
